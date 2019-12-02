942bd66744430130f9d279af725f533132498140
help97 <1204998502@qq.com>
2019-12-02 13:16:55 +0800

    第一次上传

---
+++ src/main/java/com/cy/easymallspb/EasymallSpbApplication.java
@@ -0,0 +1,13 @@
+package com.cy.easymallspb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymallSpbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasymallSpbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cy/easymallspb/EasymallSpbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy.easymallspb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymallSpbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
